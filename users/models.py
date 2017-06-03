89208ac534398f7de3d329c7368206f2b373dd56
Shun Lee <shunlee94@hotmail.com>
2017-06-03 14:54:11 +0800

    initial migrations for users app

--- users/models.py
+++ users/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Designer(models.Model):
+	
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	
+	# For python 2
+	def __unicode__(self):
+		return "%s %s" % (self.user.first_name, self.user.last_name)
+	
+	# Python 3 
+	def __str__(self):
+		return "%s %s" % (self.user.first_name, self.user.last_name)
