6625461bc7fe4637a2d98c7eac2c702ce519aac3
Sunny <sunnytam0401@gmail.com>
2017-06-04 01:37:37 +0800

    finish home page

--- factorywindow/static/js/home.js
+++ factorywindow/static/js/home.js
@@ -2,6 +2,27 @@
  * Created by Sunny on 3/6/2017.
  */
 
+function onDdlSelected(selectedFabric, selectedClothing) {
+    if(selectedFabric !== '' || selectedClothing !== '') {
+        $('.pin').hide();
+    } else {
+        $('.pin').show();
+    }
+}
+
 $(function () {
     $('.ui.dropdown').dropdown();
+
+    var selectedFabric = '';
+    var selectedClothing = '';
+
+   $('.fabric-ddl').change(function(){
+       selectedFabric = $(this).val();
+       onDdlSelected(selectedFabric, selectedClothing);
+   });
+
+    $('.clothing-ddl').change(function(){
+        selectedClothing = $(this).val();
+        onDdlSelected(selectedFabric, selectedClothing);
+   });
 });
